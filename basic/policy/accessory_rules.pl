613e5073670f725940c2cc4743d2ef93d529b361
Juho Hämäläinen <juho.hamalainen@jolla.com>
2017-02-15 15:39:40 +0200

    [common] Allow bt and ihf to coexist during call. JB#37753

--- basic/policy/accessory_rules.pl
+++ basic/policy/accessory_rules.pl
@@ -5,36 +5,22 @@
 %
 % *** bta2dp ***
 %
-% accessories that can coexists:
-%   - bthsp
-%   - tvout (audio goes to bta2dp, but video goes to tvout)
-%   - headmike
-%
 % accessories that are mutualy exclusive:
 %
 accessory_exclude( bta2dp,          headsetforcall       ).
 accessory_exclude( bta2dp,          headphoneforcall     ).
-accessory_exclude( bta2dp,          ihfforcall           ).
 
 %
 % *** bthsp ***
-%
-% accessories that can coexists:
-%   - bta2dp
-%   - tvout (audio goes to bthsp while video goes to tvout)
 %
 % accessories that are mutualy exclusive:
 %
 accessory_exclude( bthsp,           headsetforcall      ).
 accessory_exclude( bthsp,           headphoneforcall    ).
-accessory_exclude( bthsp,           ihfforcall          ).
 accessory_exclude( bthsp,           headmike            ).
 
 %
 % *** headset ***
-%
-% accessories that can coexists:
-%   - none
 %
 % accessories that are mutualy exclusive:
 %
@@ -45,9 +31,6 @@ accessory_exclude( headset, headmike  ).
 
 %
 % *** headphone ***
-%
-% accessories that can coexists:
-%   - none
 %
 % accessories that are mutualy exclusive:
 %
@@ -60,11 +43,7 @@ accessory_exclude( headphone, headmike ).
 % *** tvout ***
 %
 % accessories that can coexists:
-%   - bta2dp
-%   - bthsp
 %
-% accessories that can coexists:
-%   
 accessory_exclude( tvout, headset   ).
 accessory_exclude( tvout, headphone ).
 accessory_exclude( tvout, headmike  ).
@@ -73,11 +52,7 @@ accessory_exclude( tvout, headmike  ).
 % *** headmike ***
 %
 % accessories that can coexists:
-%   - bta2dp
-%   - bthsp
 %
-% accessories that can coexists:
-%   
 accessory_exclude( headmike, headset   ).
 accessory_exclude( headmike, headphone ).
 accessory_exclude( headmike, tvout     ).
